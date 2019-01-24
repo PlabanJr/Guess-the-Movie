57bf55e80b0b574436347c8df228d79be5822da9
PlabanJr <plabanjr@gmail.com>
2019-01-25 00:11:13 +0530

    first commit

---
+++ Game.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+class Game
+{
+        int wrongGuess;
+        char[] wrongGuessBuffer;
+        int correctGuess;
+        char[] correctGuessBuffer;
+        String movieName;
+        char guess;
+        
+        public Game(String movieName)
+        {
+            this.movieName = movieName;
+            this.wrongGuess = 0;
+            this.correctGuess = 0;
+            correctGuessBuffer = new char[this.movieName.length()];
+            wrongGuessBuffer = new char[10];
+            Arrays.fill(wrongGuessBuffer,' ');
+            Arrays.fill(correctGuessBuffer, '_');
+
+            printPart1();
+            printPart2();
+        }
+
+        public void getGuessLetter()
+        {
+            System.out.print("Guess a letter: ");
+            Scanner input = new Scanner(System.in);
+            String temp = input.nextLine();
+            if(temp.length() > 1 || (temp.charAt(0) != '\'' && !Character.isLetter(temp.charAt(0)) && !Character.isDigit(temp.charAt(0)) && !Character.isWhitespace(temp.charAt(0))))
+            {
+                System.out.println("\nInvalid guess! Enter a valid guess!\n");
+                getGuessLetter();
+            }
+            else
+            {
+                guess = temp.charAt(0);
+            }
+        }
+
+        public boolean isCorrectGuess()
+        {
+            int wrongCount = 0;
+            for(int idx = 0;idx < movieName.length();idx++)
+            {
+                if(guess == movieName.charAt(idx))
+                {
+                    if(correctGuessBuffer[idx] != guess)
+                    {
+                        correctGuessBuffer[idx] = guess;
+                        correctGuess++;
+                    }
+                    
+                }
+                else 
+                {
+                    int flag = 0;
+                    for(int j = 0;j < wrongGuess;j++)
+                    {
+                        if(guess == wrongGuessBuffer[j])
+                        {
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if(flag == 0)
+                        wrongCount++;
+                }
+            }
+ 
+            if(wrongCount == movieName.length())
+                return false;
+            else
+                return true;
+        }
+
+        
+
+        public void printPart2()
+        {
+            System.out.print("You have guessed (" + wrongGuess + ") wrong letters:");
+            for(int i = 0;i < wrongGuess;i++)
+            {
+                System.out.print(' ');
+                System.out.print(wrongGuessBuffer[i]);
+            }
+            System.out.println();
+        }
+
+        public void printPart1()
+        {
+            System.out.print("\nYou are guessing: ");
+            for(int i = 0;i < movieName.length();i++)
+            {
+                System.out.print(correctGuessBuffer[i]);
+            }
+            System.out.println();
+        }
+}
---
+++ GuessTheMovie.java
@@ -0,0 +1,101 @@
+import java.io.FileReader;
+import java.io.*;
+import java.util.*;
+
+class GuessTheMovie
+{
+    static String movieName;
+    public static void main(String[] args)
+    {
+        movieName = new String();
+        welcomeNote();
+        movieName = getMovieName();
+        // System.out.println(movieName);
+        if(!movieName.equals("Not Found"))
+            playGame();
+    }
+
+    public static void welcomeNote() 
+    {
+        System.out.println("~~~Java GuessTheMovie~~~\n");
+        System.out.println("** RULES:");
+        System.out.println("-> Guess one letter at time keeping in mind the length of the movie.");
+        System.out.println("-> If a correct letter is guessed, it will appear at it's correct position.");
+        System.out.println("-> If a wrong letter is guessed, it will increase your wrong count.");
+        System.out.println("-> Once a letter is correctly or wrongly guessed, guessing it again will make no difference.");
+        System.out.println("-> For a movie name with multiple names, you are supposed to guess the spaces as well.");
+        System.out.println("-> If your wrong count reaches 10, the game is over.\n\n");
+    }
+
+    public static String getMovieName() 
+    {
+        try 
+        {
+            movieName = getRandomMovie(new File("movieList.txt"));
+        } 
+        catch (FileNotFoundException fe) 
+        {
+            System.out.println("File not found"); 
+            movieName = "Not Found";
+            System.exit(1);
+        }
+
+        return movieName;
+    }
+
+    public static String getRandomMovie(File file) throws FileNotFoundException
+    {
+        String randomMovieName = null;
+        // Random rand = new Random();
+        Random rand = new Random(System.currentTimeMillis());
+        int count = 0;
+        for(Scanner sc = new Scanner(file); sc.hasNext(); )
+        {
+            count = rand.nextInt(25) + 1;
+            String line = sc.nextLine();
+            if(rand.nextInt(count) == 0)
+                randomMovieName = line;         
+        }
+        
+        return randomMovieName;      
+    }
+
+    public static void printWiningNote()
+    {
+        System.out.println("\nYou Win!");
+        System.out.println("You have guessed '" + movieName + "' correctly.");
+    }
+
+    public static void printLosingNote()
+    {
+        System.out.println("\nYou Lose!");
+        System.out.println("You failed to guess '" + movieName + "'.");
+    }
+
+    public static void playGame()
+    {
+        Game guessTheMovie = new Game(movieName);
+
+        while(guessTheMovie.wrongGuess < 10 && guessTheMovie.correctGuess < movieName.length())
+        {
+            guessTheMovie.getGuessLetter();
+
+            if(!guessTheMovie.isCorrectGuess())
+            {
+                guessTheMovie.wrongGuessBuffer[guessTheMovie.wrongGuess] = guessTheMovie.guess;
+                guessTheMovie.wrongGuess++;
+            }
+
+            if(guessTheMovie.correctGuess < movieName.length())
+            {
+                guessTheMovie.printPart1();
+                guessTheMovie.printPart2();
+            }
+        }
+     
+        if(guessTheMovie.correctGuess == movieName.length())
+            printWiningNote();
+        else
+            printLosingNote();
+    }
+}
